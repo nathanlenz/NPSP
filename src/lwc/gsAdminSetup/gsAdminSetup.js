56aa8374b04b76d4781d04e9a1022acfa9fc3d29
Felipe Parodi <fparodi@salesforce.com>
2020-10-20 15:47:23 -0300

    [GS] Checklist Section Items Fix Code Review v1

--- src/lwc/gsAdminSetup/gsAdminSetup.js
+++ src/lwc/gsAdminSetup/gsAdminSetup.js
@@ -3,13 +3,19 @@ import gsAdminSetupTitle from '@salesforce/label/c.gsAdminSetupTitle';
 import getChecklist from '@salesforce/apex/GS_AdminSetup.getChecklist';
 import translate from './labelTranslate';
 
-
 /*
 * @description This class control all the Admin Checklist information
 */
 export default class gsAdminSetup extends LightningElement {
+
+    /*
+    *   @description This var contain a list of checklist to render
+    */
     @track checklists;
 
+    /*
+    * @description This function load and translate all checklist sections and his items
+    */
     @wire(getChecklist)
     wiredGetChecklist({ data, error }) {
         // Hold on to the provisioned value so we can refresh it later.
@@ -18,6 +24,11 @@ export default class gsAdminSetup extends LightningElement {
         }
     }
 
+    /*
+    * @description This method translate all text info in the checklist section and his items
+    * @arg ChecklistSection
+    * @return ChecklistSection (translate)
+    */
     translateSection = (section) => {
         const tSection = {...section}
         tSection.title = translate(section.title);
@@ -26,19 +37,24 @@ export default class gsAdminSetup extends LightningElement {
         return tSection
     }
 
+    /*
+    * @description This method translate all text info in checklist items
+    * @arg ChecklistItem
+    * @return ChecklistItem (translate)
+    */
     translateSectionItem = (item) => {
         const tItem = {...item};
         tItem.title = translate(item.title);
         tItem.description = translate(item.description);
-        if(item.principalBtn) {
+        if (item.principalBtn) {
             tItem.principalBtn = {...(item.principalBtn)};
             tItem.principalBtn.label = translate(item.principalBtn.label);
         }
-        if(item.secondaryBtn) {
+        if (item.secondaryBtn) {
             tItem.secondaryBtn = {...(item.secondaryBtn)};
             tItem.secondaryBtn.label = translate(item.secondaryBtn.label);
         }
-        if(item.link) {
+        if (item.link) {
             tItem.link = {...(item.link)};
             tItem.link.label = translate(item.link.label);
         }
